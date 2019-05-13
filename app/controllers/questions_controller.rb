f86acba7d9749410c63d2484c4a26947b6480b5f
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-13 14:59:07 +0200

    got the answer logic working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,20 @@
 class QuestionsController < ApplicationController
+
   def ask
 
-    # @question =
   end
 
   def answer
     @question = params[:question]
     @answer = params[:answer]
 
-
+    if @question == 'I am going to work'
+      @answer = 'Great'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don t care, get dressed and go to work!'
+    end
   end
 end
 
-
-# If the message is I am going to work, the coach will answer Great!
-# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
-# Otherwise he will answer I don't care, get dressed and go to work!
-
-#   answer = coach_answer(your_message)
-#   if answer != ""
-#     if your_message.upcase == your_message
-#       "I can feel your motivation! #{answer}"
-#     else
-#       answer
-#     end
-#   else
-#     ""
-#   end
